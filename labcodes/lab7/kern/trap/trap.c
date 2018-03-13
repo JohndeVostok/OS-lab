61bcc2075730b258e8bd7af7840015dcbcfcc01a
Zixuan <dyxdy@live.com>
2018-03-13 08:52:29 +0800

    revert sem

--- labcodes/lab7/kern/trap/trap.c
+++ labcodes/lab7/kern/trap/trap.c
@@ -246,7 +246,7 @@ trap_dispatch(struct trapframe *tf) {
          */
 		ticks++;
 		assert(current != 0);
-		sched_class_proc_tick(current);
+		run_timer_list();
 		break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
